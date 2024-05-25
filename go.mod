354f190f1a16dc6b6196a870cacf90fec568f399
zwalo <sehoon9867@gmail.com>
2024-05-25 17:43:00 +0900

    Feat:keystore

--- go.mod
+++ go.mod
@@ -2,9 +2,38 @@ module github.com/zwalo/z-terra
 
 go 1.20
 
-require github.com/urfave/cli v1.22.15
+require (
+	github.com/codahale/sss v0.0.0-20160501174526-0cb9f6d3f7f1
+	github.com/ethereum/go-ethereum v1.13.14
+	github.com/urfave/cli v1.22.15
+	github.com/zwalo/z-nektar v1.0.2
+)
 
 require (
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/peterh/liner v1.1.1-0.20190123174540-a2c9a5303de7 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
---
+++ keystore/keystore.go
@@ -0,0 +1,288 @@
+package keystore
+
+import (
+	"crypto/aes"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/codahale/sss"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/console/prompt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/urfave/cli"
+	log "github.com/zwalo/z-nektar/zlog"
+	"github.com/zwalo/z-terra/flag"
+)
+
+const (
+	ShareJsonScheme = "passphrase"
+	ShareJsonFile   = "share.sss"
+)
+
+type ShareJson struct {
+	Index byte
+	Share []byte
+}
+
+func Command(ctx *cli.Context) error {
+	var privateKey *ecdsa.PrivateKey
+	var err error
+	isNew := ctx.GlobalBool(flag.NewFlage.Name)
+	log.Head("start generate keystore")
+	if isNew {
+		log.Trace("you chose to generate new key")
+		privateKey, err = crypto.GenerateKey()
+		if err != nil {
+			return err
+		}
+
+		path, err := prompt.Stdin.PromptInput("Enter path to save your private key file: ")
+		if err != nil {
+			return err
+		}
+
+		if err = crypto.SaveECDSA(path, privateKey); err != nil {
+			if err = os.RemoveAll(path); err != nil {
+				return err
+			} else if err = os.MkdirAll(filepath.Dir(path), 0700); err != nil {
+				return err
+			} else if err = crypto.SaveECDSA(path, privateKey); err != nil {
+				return err
+			}
+		}
+
+		log.Debug(fmt.Sprintf("success save new private key file (path : %s)", path))
+	} else {
+		log.Trace("you chose to generate with your private key")
+		isLoadFile, err := prompt.Stdin.PromptConfirm("Will you load your private key file?")
+		if err != nil {
+			return err
+		}
+		if isLoadFile {
+			log.Trace("you chose to load your private key file")
+		load_file:
+			filePath, err := prompt.Stdin.PromptInput("Enter your private key file path: ")
+			if err != nil {
+				return err
+			}
+			privateKey, err = crypto.LoadECDSA(filePath)
+			if err != nil {
+				log.Error("Failed to load private key file: %v", err)
+				goto load_file
+			}
+			log.Debug("success load private key file")
+		} else {
+			log.Trace("you chose to enter your private key")
+		enter_pk:
+			privateKeyHex, err := prompt.Stdin.PromptInput("Enter your private key (without 0x prefix): ")
+			if err != nil {
+				return err
+			}
+
+			privateKey, err = crypto.HexToECDSA(privateKeyHex)
+			if err != nil {
+				log.Error("Failed to convert private key: %v", err)
+				goto enter_pk
+			}
+
+			log.Debug("success enter private key")
+		}
+	}
+
+	// Create a new Ethereum keystore
+	path, err := prompt.Stdin.PromptInput("Enter your new keystore path: ")
+	if err != nil {
+		return err
+	}
+	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+
+	// Get passphrase sss or single
+	passphrase, err := makePassphrase()
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to make passphrase: %v", err))
+		return err
+	}
+
+	account, err := ks.ImportECDSA(privateKey, passphrase)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to import private key into keystore: %v", err))
+		return err
+	}
+
+	log.Info(fmt.Sprintf("keystore file created for Ethereum address: %s", account.Address.Hex()))
+	return nil
+}
+
+func makePassphrase() (string, error) {
+enter_split:
+	if num, err := prompt.Stdin.PromptInput("How many share of password? if less than 2, a normal password is generated. <splitN> : "); err != nil {
+		return "", err
+	} else if num == "" {
+		goto enter_split
+	} else if splitN, err := strconv.Atoi(num); err != nil {
+		return "", err
+	} else if splitN >= 10 || splitN < 0 {
+		log.Warn("splitN is a positive integer less than 10.")
+		goto enter_split
+	} else {
+		var (
+			passphrase string
+			err        error
+		)
+		switch {
+		case splitN > 1:
+			passphrase, err = sssPassphrase(splitN)
+		default:
+			passphrase, err = singlePassphrase()
+		}
+
+		if err != nil {
+			return "", err
+		}
+
+		return passphrase, nil
+	}
+}
+
+func sssPassphrase(splitN int) (string, error) {
+	log.Trace(fmt.Sprintf("generate sss (split count : %d)", splitN))
+
+enter_threshold:
+	if num, err := prompt.Stdin.PromptInput(fmt.Sprintf("Enter the threshold, must be less than or equal to %v. <threshold>: ", splitN)); err != nil {
+		return "", nil
+	} else if num == "" {
+		goto enter_threshold
+	} else if threshold, err := strconv.Atoi(num); err != nil {
+		return "", nil
+	} else if threshold <= 1 || threshold > splitN {
+		log.Warn("threshold must be greater than 1 and less than splitN.")
+		goto enter_threshold
+	} else {
+		var (
+			passphrase string
+			shares     map[byte][]byte
+			err        error
+		)
+
+	confirm_passphrase:
+		yes, err := prompt.Stdin.PromptConfirm("Are you going to set this split keystore to be single unlockable?: ")
+		if err != nil {
+			return "", nil
+		} else {
+			if yes {
+				log.Trace("you chose that SSS key can be unlockable by single passphrase (128bit or 192bit or 256bit)")
+				if passphrase, err = singlePassphrase(); err != nil {
+					return "", nil
+				} else {
+					if !(len(passphrase) == 16 || len(passphrase) == 24 || len(passphrase) == 32) {
+						log.Warn("passphrase size must be 128bit or 192bit or 256bit")
+						goto confirm_passphrase
+					}
+				}
+
+			} else {
+				log.Trace("you chose that SSS key can be not unlockable by single passphrase")
+				passphrase, err = randPassphrase(32)
+				if err != nil {
+					return "", err
+				}
+			}
+		}
+
+		if shares, err = sss.Split(byte(splitN), byte(threshold), []byte(passphrase)); err != nil {
+			return "", err
+		}
+
+		log.Debug(fmt.Sprintf("passphrase splited to %d (threshold=%d / single unlock = %v)", len(shares), threshold, yes))
+
+		usbs := make(map[string]int)
+		usbIndex := make(map[byte]string)
+		for i := 1; i <= splitN; i++ {
+		enter_path:
+			if usbPath, err := prompt.Stdin.PromptInput(fmt.Sprintf("Enter the path to store the share (%d/%d) : ", i, splitN)); err != nil {
+				return "", err
+			} else if usbPath == "" {
+				goto enter_path
+			} else {
+				usb := strings.TrimSpace(usbPath)
+				if _, ok := usbs[usb]; ok {
+					return "", fmt.Errorf("found equal usb directory")
+				} else {
+					usbs[usb] = i
+					usbIndex[byte(i)] = usb
+
+					if err := writeFile(usb, &ShareJson{Index: byte(i), Share: shares[byte(i)]}); err != nil {
+						return "", err
+					}
+					log.Trace(fmt.Sprintf("%d share of passphrase transfered to %s", i, usb))
+				}
+			}
+		}
+		return passphrase, nil
+	}
+}
+
+func singlePassphrase() (string, error) {
+	passphrase, err := prompt.Stdin.PromptPassword("Enter your passphrase: ")
+	return passphrase, err
+}
+
+func randPassphrase(n int) (string, error) {
+	var runes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+{}|[]")
+	b := make([]rune, n)
+	max := new(big.Int).SetUint64(uint64(len(runes)))
+	for i := range b {
+		if n, err := rand.Int(rand.Reader, max); err != nil {
+			return "", nil
+		} else {
+			b[i] = runes[n.Uint64()]
+		}
+
+	}
+	return string(b), nil
+}
+
+func writeFile(dir string, shareJson *ShareJson) error {
+	if err := encrypt(shareJson); err != nil {
+		log.Error("share key json encrypt error, ", err)
+		return err
+	}
+
+	dir = strings.TrimSpace(dir)
+	endPath := path.Join(dir, ShareJsonScheme)
+	if err := os.RemoveAll(endPath); err != nil {
+		return err
+	} else if err := os.MkdirAll(endPath, 0700); err != nil {
+		return err
+	} else if bytes, err := json.Marshal(shareJson); err != nil {
+		return err
+	} else if err := os.WriteFile(path.Join(endPath, ShareJsonFile), bytes, 0700); err != nil {
+		return err
+	}
+	return nil
+}
+
+func encrypt(shareJson *ShareJson) error {
+	if passphrase, err := prompt.Stdin.PromptPassword("Enter the password of new share: "); err != nil {
+		return err
+	} else {
+		key := crypto.Keccak256([]byte(passphrase))[:len(shareJson.Share)]
+
+		if block, err := aes.NewCipher([]byte(key)); err != nil {
+			return err
+		} else {
+			ciphertext := make([]byte, block.BlockSize())
+			block.Encrypt(ciphertext, shareJson.Share)
+			shareJson.Share = ciphertext
+		}
+	}
+	return nil
+}
